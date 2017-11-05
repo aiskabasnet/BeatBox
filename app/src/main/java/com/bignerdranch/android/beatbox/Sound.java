26986d3ea5498d2e9973c3dd263380424b9445a9
Aiska Basnet <aiska.basnet@gmail.com>
2017-11-05 08:08:27 +0545

    First Commit

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
@@ -0,0 +1,42 @@
+package com.bignerdranch.android.beatbox;
+
+/**
+ * Created by aiska on 11/2/17.
+ */
+
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+    private Integer mSoundId;
+    public Sound(String assetPath){
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length-1];
+        mName = filename.replace(".wav","");
+    }
+
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
+    }
+
+    public void setAssetPath(String assetPath) {
+        mAssetPath = assetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+}
